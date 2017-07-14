7be103697226ebdd4c799fb81897f64c6c818a46
Mufaddal Motorwala <mufaddal.motorwala@gmail.com>
2017-07-14 13:09:51 +0400

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
+// Static server
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./src"
+        }
+    });
+    gulp.watch("./src/**/*").on("change", reload);
+});
